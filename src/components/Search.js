d388665d29ff7c0083dce8a462a514b419690e3c
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-10-12 20:54:34 +0100

    added styling and logic to search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import "../styles/Search.css"
 
 const Search = () => {
-
+const [value, setValue] = useState("")
 
     return (
-        <> <input className="search_input" type="text"/> </>
+        <> 
+        <form className="search_form">
+        <input onChange={(e) => setValue(e.target.value)} className="search_input" type="text"/> 
+        <button className="submit_button" type="submit" placeholder="Search our cool database.."> Search </button>
+        </form> </>
     )
 
     }
