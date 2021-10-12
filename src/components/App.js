31939d80985a866f7577904609a14ff5f8cf3f1e
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-10-12 20:15:18 +0100

    added search component and NASA logo, with some styling. Also checked that the App and Search component rendered correctly

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/App.css';
+import Search from './Search'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <img className="NASA_logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt="NASALogo"/>
+      <Search />
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "../styles/Search.css"
+
+const Search = () => {
+
+
+    return (
+        <> <input className="search_input" type="text"/> </>
+    )
+
+    }
+
+    export default Search;
